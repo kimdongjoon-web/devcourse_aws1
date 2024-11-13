a02c34cb3bc1f9b8fdab3cd5ed468be1d06131da
kimdongjoon-web <color6763@naver.com>
2024-11-13 09:12:59 +0900

    test: test 설정 추가

--- src/test/java/com/example/devcourse_aws1/DevcourseAws1ApplicationTests.java
+++ src/test/java/com/example/devcourse_aws1/DevcourseAws1ApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.devcourse_aws1;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:/application-test.properties")
 class DevcourseAws1ApplicationTests {
 
     @Test
