b5c9be5f1791f87beab1af5c909c23a0668869c0
kimdongjoon-web <color6763@naver.com>
2024-11-13 09:12:39 +0900

    feat: 검증 기능 추가

--- src/main/java/com/example/devcourse_aws1/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/devcourse_aws1/domain/user/repository/UserRepository.java
@@ -4,4 +4,8 @@ import com.example.devcourse_aws1.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByLoginId(String loginId);
+    boolean existsByEmail(String email);
+    boolean existsByPhone(String phone);
 }
